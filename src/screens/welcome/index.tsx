4a39207fff41e349bb419d966e960069c885e259
Ronny Freites <ronnyfreites@Ronnys-MacBook-Pro.local>
2020-01-07 19:37:22 -0600

    add Appbar to Welcome page

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import Container from '@material-ui/core/Container'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Button from '@material-ui/core/Button'
+import Box from '@material-ui/core/Box'
 
 import ProfileCard from 'components/profile-card'
 import useFackeData from 'fake'
@@ -15,21 +19,27 @@ function Welcome() {
   const [dataUsers, setDataUsers] = useState([])
 
   useEffect(() => {
-    getDataUsers(500)
+    getDataUsers(96)
       .then(({results}: any) => setDataUsers(results))
   }, [])
 
   return (
     <Container>
       <CssBaseline />
-      <Link to="/login">
-        Already have an account? Sign in
-      </Link>
-      <Grid item xs={12}>
-        <Grid container justify="center" spacing={2}>
-          {dataUsers.map(({picture, login}: any) => <ProfileCard key={login.uuid} image={picture.large} />)}
+      <AppBar position='static' color='default'>
+        <Toolbar>
+          <Button component={ Link } to="/login" variant="outlined" color="secondary">
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Box mt={2}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={1}>
+            {dataUsers.map(({picture, login}: any) => <ProfileCard key={login.uuid} image={picture.large} />)}
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Container>
   )
 }
