171b3c37efdf8e8700e6cd70811c4cfba5a3df51
RONNY FREITES <ronnyangelo.freites@gmail.com>
2020-03-22 11:43:47 -0600

    update production env

--- src/config/env.tsx
+++ src/config/env.tsx
@@ -28,17 +28,14 @@ const ENV = {
   production: {
     aws: {
       Auth: {
-        identityPoolId: '',
-        region: '',
-        userPoolId: '',
-        userPoolWebClientId: '',
-      },
-      Storage: {
-        bucket: '',
+        identityPoolId: 'us-east-1:10d55aba-74c6-4581-957e-8703b535d011',
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_HFHwlhVNT',
+        userPoolWebClientId: '49la7dn3p80baqt8dkpp4ojqci',
       },
     },
     coreApi: {
-      uri: '',
+      uri: 'https://consumopymecr.herokuapp.com',
     },
   },
 }
